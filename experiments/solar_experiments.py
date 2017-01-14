053e0616e4513b1006a5db8b04c01040bd27ef5f
david-abel <david_abel@brown.edu>
2017-01-14 12:49:32 -0500

    Merging changes to experiments file

--- experiments/solar_experiments.py
+++ experiments/solar_experiments.py
@@ -16,27 +16,35 @@ from simple_rl.agents import RandomAgent, FixedPolicyAgent, QLearnerAgent, Linea
 
 # Local imports.
 from solarOOMDP.SolarOOMDPClass import SolarOOMDP
+from SolarTrackerClass import SolarTracker
 import tracking_baselines as tb
 
 def main():
+
+
     # Setup MDP.
-    solar_mdp = SolarOOMDP()
+    panel_step = 1
+    solar_mdp = SolarOOMDP(timestep=5, panel_step=panel_step)
     actions = solar_mdp.get_actions()
     gamma = solar_mdp.get_gamma()
 
     # Setup fixed agents.
-    baseline_policy = tb.policy_from_simple_tracker
-    baseline_tracker_agent = FixedPolicyAgent(baseline_policy)
+    tracker = SolarTracker(tb.simple_tracker, panel_step=panel_step)
+    baseline_tracker_agent = FixedPolicyAgent(tracker.get_policy())
+
+    # Setup fixed agents.
+    static_agent = FixedPolicyAgent(tb.static_policy)
+
 
     # Setup RL agents.
     random_agent = RandomAgent(actions)
     qlearner_agent = QLearnerAgent(actions, gamma=gamma, explore="uniform")
-    lin_approx_agent = LinearApproxQLearnerAgent(actions, gamma=gamma)
+    lin_approx_agent = LinearApproxQLearnerAgent(actions, gamma=gamma, rbf=True)
 
-    agents = [qlearner_agent, random_agent, baseline_tracker_agent]
+    agents = [lin_approx_agent, static_agent, random_agent]
     
     # Run experiments.
-    run_agents_on_mdp(agents, solar_mdp, num_instances=3, num_episodes=500, num_steps=80)
+    run_agents_on_mdp(agents, solar_mdp, num_instances=3, num_episodes=1, num_steps=24*6*5)
 
 if __name__ == "__main__":
     main()
